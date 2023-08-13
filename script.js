28aad5582e947cb4f8a81e48585cd3b044d175a5
Ai Anshu <gxanshu@yahoo.com>
2023-08-13 14:16:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,142 +1,192 @@
+// get today's date
 var date = new Date();
-var inputTitle = document.getElementById('tasktitle');
-var inputDis = document.getElementById('taskdescription');
-var listContainer = document.getElementById('todobox');
-let listFilterdCountPercentage = document.getElementById('percentage');
-let listFilterdCount = document.getElementById('complate');
+// all the elements
+var inputTitle = document.getElementById("tasktitle");
+var inputDis = document.getElementById("taskdescription");
+var listContainer = document.getElementById("todobox");
+let listFilterdCountPercentage = document.getElementById("percentage");
+let listFilterdCount = document.getElementById("complate");
 var tasks = [];
 
-// If vilue is null the add some text.
+// if application run for the first time then it will return null so initilizing localStorage to save tasks
 if (localStorage.getItem("storage") == null) {
-    localStorage.setItem("storage", '[]');
+  localStorage.setItem("storage", "[]");
 }
 
-// Set Noone
-let nooneHoure = date.toLocaleString('en-US', { hour12: false, hour: '2-digit' });
-let noone = document.getElementById('noone');
-if (nooneHoure >= parseInt("06", 8) && nooneHoure <= 11) {
-    noone.innerHTML = 'morning';
-} else if (nooneHoure >= 12 && nooneHoure <= 17) {
-    noone.innerHTML = 'afternoon';
-} else if (nooneHoure >= 18 && nooneHoure <= 24) {
-    noone.innerHTML = 'evening';
+// setting greeting with time
+// gettting time in 01-24 hours format
+let noonHour = date.toLocaleString("en-US", { hour12: false, hour: "2-digit" });
+// getting the element from the DOM
+let noon = document.getElementById("noone");
+// if time is bigger than 06 and smaller then 11
+if (noonHour >= parseInt("06", 8) && noonHour <= 11) {
+  noon.innerHTML = "morning";
+  // if time is bigger than 12 and smaller than 05
+} else if (noonHour >= 12 && noonHour <= 17) {
+  noon.innerHTML = "afternoon";
+  // if time is bigger than 05 and smaller than 12 PM
+} else if (noonHour >= 18 && noonHour <= 24) {
+  noon.innerHTML = "evening";
+  // in the remainning time from 12AM to 06 AM its night
 } else {
-    noone.innerHTML = 'night';
+  noon.innerHTML = "night";
 }
+
+// getting todays date
+let todaysDate = date.toLocaleString("en-India", {
+  // this will give months in short form eg August -> Aug
+  month: "short",
+  // todays date in two digit format like 03 or 11
+  day: "2-digit",
+});
 
 // Change the Date
-document.getElementById('progress-date').innerText = date.toLocaleString('en-India', { month: 'short', day: '2-digit', });
+document.getElementById("progress-date").todaysDate;
 
-
-// function's
+// function run to cut line effect in the dom just need to give the id
 function cutline(id) {
-    document.getElementById(id).style.textDecoration = 'line-through';
+  // add new style as text-decoration: line-thorugh
+  document.getElementById(id).style.textDecoration = "line-through";
 }
 
+// function to open the modal to add tasks
 function addTask() {
-    let addTask = document.getElementsByClassName('addtask-hide');
-    addTask[0].className = 'addtask';
+  let addTask = document.getElementsByClassName("addtask-hide");
+  // adding addTask class in the element
+  addTask[0].className = "addtask";
 }
 
+// to hide the task
 function hideTask() {
-    let addTask = document.getElementsByClassName('addtask');
-    addTask[0].className = 'addtask-hide';
+  let addTask = document.getElementsByClassName("addtask");
+  // adding addtask-hide class in the element
+  addTask[0].className = "addtask-hide";
 }
 
+// function to add new task on the DOM as well as in the last list (Array)
 function addNewTask() {
-    if (inputTitle.value == '' && inputDis.value == '') {
-        alert('You can\'t crate emptey todo!');
-    } else {
-        tasks.unshift({
-            title: inputTitle.value,
-            description: inputDis.value,
-            checked: false
-        })
-        render();
-        prograssStatus();
-    }
-    hideTask();
-    inputTitle.value = '';
-    inputDis.value = '';
+  // checking if user enter any value of not in the input box
+  if (inputTitle.value == "" && inputDis.value == "") {
+    // run if user not enter any task in the input box
+    alert("You can't crate emptey todo!");
+  } else {
+    // if user add the task in inputbox
+    // The unshift() method adds the specified elements to the beginning of an array and returns the new length of the array.
+    tasks.unshift({
+      // setting values as the user input
+      title: inputTitle.value,
+      description: inputDis.value,
+      checked: false,
+    });
+    // this function render new html with the help of array
+    render();
+    // this function render the progress bar
+    prograssStatus();
+  }
+  // hide the modal when user click on save
+  hideTask();
+  // replace input values to empty
+  inputTitle.value = "";
+  inputDis.value = "";
 }
 
 // Render Function
 function render() {
-    let innerHTML = "";
-    for (let i = 0; i < tasks.length; i++) {
-        if (tasks[i].checked === true) {
-            innerHTML += `<li>
+  let innerHTML = "";
+  // run as the length of the tasks array
+  for (let i = 0; i < tasks.length; i++) {
+    // if task in checked (done) then render as done
+    if (tasks[i].checked === true) {
+      innerHTML += `<li>
             <button onclick="unchecked(${i})"><i class="material-symbols-outlined">done</i></button>
             <div class="taskDetail" title="Click to view details" onclick="details(${i})">
             <h1 style="color: #9b9b9b;text-decoration: line-through;">${tasks[i].title}</h1>
             <p style="color: #9b9b9b;text-decoration: line-through";>${tasks[i].description}</p>
             </div>
-            </li>`
-        } else {
-            innerHTML += `<li>
+            </li>`;
+    } else {
+      innerHTML += `<li>
         <button onclick="checked(${i})" style="padding: 13px;"></button>
         <div class="taskDetail" title="Click to view details" onclick="details(${i})">
             <h1>${tasks[i].title}</h1>
             <p>${tasks[i].description}</p>
         </div>
-        </li>`
-        }
+        </li>`;
     }
-    listContainer.innerHTML = innerHTML;
-    document.getElementById("ongoing-count").innerText = tasks.length;
-    prograssStatus();
+  }
+  // now update that html that render with array and append in listContainer
+  listContainer.innerHTML = innerHTML;
+  // update the counter
+  document.getElementById("ongoing-count").innerText = tasks.length;
+  /// change the progress bar
+  prograssStatus();
 }
-
 
 // chacked and unchacken
 function checked(id) {
-    tasks[id].checked = true;
-    render();
-    prograssStatus();
+  // set that perticular task to true
+  tasks[id].checked = true;
+  // rerender the UI
+  render();
+  // rerender the progress Bar
+  prograssStatus();
 }
 function unchecked(id) {
-    tasks[id].checked = false;
-    render();
-    prograssStatus();
+  // set that perticular task to false
+  tasks[id].checked = false;
+  // rerender the UI
+  render();
+  // rerender the progress Bar
+  prograssStatus();
 }
 
 // Show details
 function details(i) {
-    document.getElementById('changetitle').value = tasks[i].title;
-    document.getElementById('changedescription').value = tasks[i].description;
-    let addTask = document.getElementsByClassName('changetask-hide');
-    addTask[0].className = 'changetask';
+  document.getElementById("changetitle").value = tasks[i].title;
+  document.getElementById("changedescription").value = tasks[i].description;
+  let addTask = document.getElementsByClassName("changetask-hide");
+  addTask[0].className = "changetask";
 }
 function changeTask() {
-    let addTask = document.getElementsByClassName('changetask');
-    addTask[0].className = 'changetask-hide';
+  let addTask = document.getElementsByClassName("changetask");
+  addTask[0].className = "changetask-hide";
 }
-
 
 // Task compated function
 function prograssStatus() {
-    let completedTask = [];
-    for (let i = 0; i < tasks.length; ++i) {
-        if (tasks[i].checked === true) {
-            completedTask.push(tasks[i]);
-        }
+  let completedTask = [];
+  // run as the length of array
+  for (let i = 0; i < tasks.length; ++i) {
+    // getting all the completed tasks as array
+    if (tasks[i].checked === true) {
+      completedTask.push(tasks[i]);
     }
-    listFilterdCount.innerHTML = completedTask.length + '/' + tasks.length;
-    let progressPercentage = Math.floor((completedTask.length / tasks.length) * 100);
-    if (isNaN(progressPercentage)) {
-        listFilterdCountPercentage.innerHTML = 0;
-    } else {
-        listFilterdCountPercentage.innerHTML = progressPercentage;
-    }
-    document.getElementById('progress-line').style.width = progressPercentage + '%';
-    setLocalItem();
+  }
+  // setting html as completedTask length / task length
+  listFilterdCount.innerHTML = completedTask.length + "/" + tasks.length;
+  // finding progress percantage with the formula
+  let progressPercentage = Math.floor(
+    (completedTask.length / tasks.length) * 100,
+  );
+  // if get 0
+  if (isNaN(progressPercentage)) {
+    listFilterdCountPercentage.innerHTML = 0;
+  } else {
+    listFilterdCountPercentage.innerHTML = progressPercentage;
+  }
+  document.getElementById("progress-line").style.width =
+    progressPercentage + "%";
+  setLocalItem();
 }
 
 // Local Storage
+// setting all the task in the localStorage
 function setLocalItem() {
-    localStorage.setItem("storage", JSON.stringify(tasks));
+  localStorage.setItem("storage", JSON.stringify(tasks));
 }
+
+// run first when the page is load
 window.onload = function () {
-    tasks = JSON.parse(localStorage.getItem("storage"));
-    render();
-}
+  tasks = JSON.parse(localStorage.getItem("storage"));
+  render();
+};
